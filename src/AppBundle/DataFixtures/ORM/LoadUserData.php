febd5f8449c70b3d36efbae40f0922f2d29aab63
Estel Smith <estel.smith@gmail.com>
2015-04-15 16:15:33 -0400

    Added user/school relationships.

--- src/AppBundle/DataFixtures/ORM/LoadUserData.php
+++ src/AppBundle/DataFixtures/ORM/LoadUserData.php
@@ -4,24 +4,30 @@ namespace AppBundle\DataFixtures\ORM;
 
 use AppBundle\Entity\User;
 use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class LoadUserData implements FixtureInterface
+class LoadUserData implements FixtureInterface, OrderedFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $schoolRespository = $manager->getRepository('AppBundle:School');
+
         $users = [
             [
                 'username' => 'test1',
-                'password' => 'test1'
+                'password' => 'test1',
+                'school' => 'School 1'
             ],
             [
                 'username' => 'test2',
-                'password' => 'test2'
+                'password' => 'test2',
+                'school' => 'School 1'
             ],
             [
                 'username' => 'test3',
-                'password' => 'test3'
+                'password' => 'test3',
+                'school' => 'School 2'
             ]
         ];
 
@@ -29,11 +35,21 @@ class LoadUserData implements FixtureInterface
             $user = (new User())
                 ->setUsername($userData['username'])
                 ->setPassword($userData['password'])
+                ->setSchool(
+                    $schoolRespository->findOneBy([
+                        'name' => $userData['school']
+                    ])
+                )
             ;
 
             $manager->persist($user);
         }
 
         $manager->flush();
+    }
+
+    public function getOrder()
+    {
+        return 1;
     }
 }
